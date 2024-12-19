cafcc50533a99d30ed731f918575e492862eb894
kayle <kayleyu@icloud.com>
2024-12-19 21:09:51 +0800

    Initial Commit

---
+++ MyIOSProduct/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyIOSProduct
+//
+//  Created by Kayle Stark on 2024/12/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyIOSProduct/MyIOSProductApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyIOSProductApp.swift
+//  MyIOSProduct
+//
+//  Created by Kayle Stark on 2024/12/19.
+//
+
+import SwiftUI
+
+@main
+struct MyIOSProductApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
